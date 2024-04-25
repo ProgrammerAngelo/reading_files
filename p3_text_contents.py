4a4c1e9211c17a08a975a1d46cf239d23ec88f54
ProgrammerAngelo <angelovpangilagan@gmail.com>
2024-04-25 03:57:47 -0400

    Added the loop for input

--- p3_text_contents.py
+++ p3_text_contents.py
@@ -1,5 +1,9 @@
 #add a method that write multiple lines of text
 #add a method that used mylife.txt for text
+proceed = True
+while proceed:
+    line = input("Enter line: ")
+    break_line = line + ("\n")
 #add a method for looping text
 #add an input for lines
 #add a method that ask the user y or n
