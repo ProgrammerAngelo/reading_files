be233cdc8f3913d84428744bbbe6da7b1faf26c3
ProgrammerAngelo <angelovpangilagan@gmail.com>
2024-04-25 01:47:29 -0400

    Added method for reading source file

--- p2_reading_GWA.py
+++ p2_reading_GWA.py
@@ -1,5 +1,11 @@
 #add a method for reading the students name and GWA
-directory = (r"students_names_GWA.txt")
+path = (r"students_names_GWA.txt")
+def find_highest_GWA(path):
 #add a method used for reading the students txt as student source file
+    with open(path,"r") as source_file:
 #add a method for printing names
+        data = source_file.read()
+        txt_data = eval(data)
+        highest_GWA = None
+        students_highest_GWA = None
 #add a method for printing highest GWA
