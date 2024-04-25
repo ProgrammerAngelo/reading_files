de4eda2e536ce00905a21f0a4e43d5c3495d274b
ProgrammerAngelo <angelovpangilagan@gmail.com>
2024-04-24 22:45:11 -0400

    Added a method for reading numbertxt and set it as number source

--- p1_writing_numbers.py
+++ p1_writing_numbers.py
@@ -1,4 +1,5 @@
+#add a txt file used for number source
 #add a method used for reading the number source
+with open("numbers.txt","r") as number_source:#for reading the number txt as a number source
 #add a method used for finding even numbers
 #add a method for finding odd numbers
-#add a txt file used for number source
