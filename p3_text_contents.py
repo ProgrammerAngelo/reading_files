d07e51564798b1c3fa5c24a15cf2b75379a0c4d7
ProgrammerAngelo <angelovpangilagan@gmail.com>
2024-04-25 05:02:30 -0400

    Added font and color for texts

--- p3_text_contents.py
+++ p3_text_contents.py
@@ -1,8 +1,19 @@
 #add a method that write multiple lines of text
 #add a method that used mylife.txt for text
+RED = '\033[31m'
+GREEN = '\033[32m'
+YELLOW = '\033[33m'
+BRIGHT_CYAN = '\033[96m'
+WHITE = '\033[97m'
+BACKGROUND_BLACK = '\033[40m'
+RESET = '\033[0m'
+from pyfiglet import Figlet
+font = Figlet(font='drpepper')
+print(YELLOW + font.renderText("Hello User") + RESET)
+
 proceed = True
 while proceed:
-    line = input("Enter line: ")
+    line = input(BRIGHT_CYAN + BACKGROUND_BLACK + "\n Enter line: \n" + RESET)
     break_line = line+("\n")
 #add a method for looping text
     with open("my_life.txt", "a") as my_life_output:
@@ -10,11 +21,11 @@ while proceed:
 #add an input for lines
         while proceed:
 #add a method that ask the user y or n
-            repeat = input("Are there any more lines yes/no? \n")
+            repeat = input("\n"+ GREEN+BACKGROUND_BLACK +"Are there any more lines yes/no? \n"+RESET)
             if repeat.lower() == "yes":
                 break
             elif repeat.lower() == "no":
                 proceed = False
             else:
-                print("Invalid, type yes/no only bro!")
+                print(RED+BACKGROUND_BLACK+"\nInvalid, type yes/no only bro!"+ RESET)
                 continue
