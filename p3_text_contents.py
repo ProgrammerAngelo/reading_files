7aaef21570db1bdda927824df31888add822fe68
ProgrammerAngelo <angelovpangilagan@gmail.com>
2024-04-25 03:50:14 -0400

    Added pseudocode

---
+++ p3_text_contents.py
@@ -0,0 +1,5 @@
+#add a method that write multiple lines of text
+#add a method that used mylife.txt for text
+#add a method for looping text
+#add an input for lines
+#add a method that ask the user y or n
