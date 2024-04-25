b0baeadcde2fe910a9b4bd7f2012780cf878f11b
ProgrammerAngelo <angelovpangilagan@gmail.com>
2024-04-25 00:49:40 -0400

    Added pseudocode

---
+++ p2_reading_GWA.py
@@ -0,0 +1,4 @@
+#add a method for reading the students name and GWA
+#add a method used for reading the students txt as student source file
+#add a method for printing names
+#add a method for printing highest GWA
