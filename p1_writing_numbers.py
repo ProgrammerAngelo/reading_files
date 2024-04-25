278c66fb2c71d94e8f5d440d56b2df6bea083504
ProgrammerAngelo <angelovpangilagan@gmail.com>
2024-04-24 23:14:21 -0400

    Final product

--- p1_writing_numbers.py
+++ p1_writing_numbers.py
@@ -5,10 +5,10 @@ def read_write():
 #add a method used for finding even numbers
         for line in number_source:
             if int(line) % 2 == 0:
-                with open("even.txt","w") as even_output_file:
+                with open("even.txt","a") as even_output_file:
                     even_output_file.write(line)
 #add a method for finding odd numbers
             else:
-                with open("odd.txt","w") as odd_output_file:
+                with open("odd.txt","a") as odd_output_file:
                     odd_output_file.write(line)
 read_write()
