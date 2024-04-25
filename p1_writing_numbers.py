3b0f2f9edac430de8323aff11e562debcd32b7e3
ProgrammerAngelo <angelovpangilagan@gmail.com>
2024-04-24 22:49:08 -0400

    Added the method for finding the even numbers

--- p1_writing_numbers.py
+++ p1_writing_numbers.py
@@ -2,4 +2,6 @@
 #add a method used for reading the number source
 with open("numbers.txt","r") as number_source:#for reading the number txt as a number source
 #add a method used for finding even numbers
+    for line in number_source:
+        if int(line) % 2 == 0:
 #add a method for finding odd numbers
