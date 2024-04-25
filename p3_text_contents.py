9378d2482c732b3c339b63b14c31c5c097aceec2
ProgrammerAngelo <angelovpangilagan@gmail.com>
2024-04-25 04:12:17 -0400

    Changed my_life.txt to my_life_output

--- p3_text_contents.py
+++ p3_text_contents.py
@@ -5,5 +5,7 @@ while proceed:
     line = input("Enter line: ")
     break_line = line + ("\n")
 #add a method for looping text
+    with open("my_life.txt") as my_life_output:
+        my_life_output.write(break_line)
 #add an input for lines
 #add a method that ask the user y or n
