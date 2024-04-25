70c6b7d0b51666c00231b61d8eb638fc42ea3eb3
ProgrammerAngelo <angelovpangilagan@gmail.com>
2024-04-25 05:35:16 -0400

    Added pseudocode

---
+++ p4_squared_cube_of_integers.py
@@ -0,0 +1,5 @@
+#add a method thad read the source file
+#make a txt file that named integers
+#add a method that reads the txt as number source
+#add a method that finds the squared of the inetgers
+#add a method that finds the cube of the inetgers
