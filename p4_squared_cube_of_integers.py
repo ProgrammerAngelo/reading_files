c9e7592ee1e832be78231cd06d2b7349c31555f5
ProgrammerAngelo <angelovpangilagan@gmail.com>
2024-04-25 05:44:29 -0400

    Added the txt files and added numbers in integers.txt file

--- p4_squared_cube_of_integers.py
+++ p4_squared_cube_of_integers.py
@@ -1,5 +1,5 @@
 #add a method thad read the source file
-#make a txt file that named integers
+#make a txt file that named integers.txt, double.txt, triple.txt
 #add a method that reads the txt as number source
 #add a method that finds the squared of the inetgers
 #add a method that finds the cube of the inetgers
