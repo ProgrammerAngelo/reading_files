1677c4d141308def26420f0a617482b2da0a791f
ProgrammerAngelo <angelovpangilagan@gmail.com>
2024-04-24 22:56:26 -0400

    Added the method for writing evennumbers txt

--- p1_writing_numbers.py
+++ p1_writing_numbers.py
@@ -4,4 +4,6 @@ with open("numbers.txt","r") as number_source:#for reading the number txt as a n
 #add a method used for finding even numbers
     for line in number_source:
         if int(line) % 2 == 0:
+            with open("even.txt","a") as even_output_files:
+                even_output_files.write(line)
 #add a method for finding odd numbers
