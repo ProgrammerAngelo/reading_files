481934ff8ad06ab49f524ec33be0362db5f1e575
ProgrammerAngelo <angelovpangilagan@gmail.com>
2024-04-24 23:04:21 -0400

    Added a method for writing even and odd numbers

--- p1_writing_numbers.py
+++ p1_writing_numbers.py
@@ -1,12 +1,14 @@
 #add a txt file used for number source
 #add a method used for reading the number source
-with open("numbers.txt","r") as number_source:#for reading the number txt as a number source
+def read_write():
+    with open("numbers.txt","r") as number_source:#for reading the number txt as a number source
 #add a method used for finding even numbers
-    for line in number_source:
-        if int(line) % 2 == 0:
-            with open("even.txt","w") as even_output_file:
-                even_output_file.write(line)
+        for line in number_source:
+            if int(line) % 2 == 0:
+                with open("even.txt","w") as even_output_file:
+                    even_output_file.write(line)
 #add a method for finding odd numbers
-        else:
-            with open("odd.txt","w") as odd_output_file:
-                odd_output_file.write(line)
+            else:
+                with open("odd.txt","w") as odd_output_file:
+                    odd_output_file.write(line)
+read_write()
