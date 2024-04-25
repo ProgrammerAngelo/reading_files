eae7ca3c4db5e4a3a64c32dae1ec0348d7f1c323
ProgrammerAngelo <angelovpangilagan@gmail.com>
2024-04-25 01:38:34 -0400

    Change the students_names_GWA.txt to path

--- p2_reading_GWA.py
+++ p2_reading_GWA.py
@@ -1,4 +1,5 @@
 #add a method for reading the students name and GWA
+directory = (r"students_names_GWA.txt")
 #add a method used for reading the students txt as student source file
 #add a method for printing names
 #add a method for printing highest GWA
